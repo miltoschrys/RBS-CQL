41f9f292657e0df8264eb231f7f0d336846f801c
miltoschrys <34551870+miltoschrys@users.noreply.github.com>
2024-05-22 15:12:15 +0100

    Delete temp.py

--- temp.py
+++
@@ -1,3 +0,0 @@
-kubectl get --all-namespaces  --output json  pods | jq '.items[] | select(.status.podIP=="10.22.19.69")' | jq .metadata.name
-step 20 remove from dataset
-remove 74 ste pfrom dataset
